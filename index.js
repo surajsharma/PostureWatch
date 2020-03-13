7013077379ab2201e52532591cfd9ca159807a6a
suraj <surajsharma@pm.me>
2020-03-13 21:16:40 +0530

    init

---
+++ index.js
@@ -0,0 +1,64 @@
+const anybar = require("anybar");
+const Timer = require("tiny-timer");
+const random = require("random");
+const readline = require("readline");
+const prompt = require("prompt");
+const { exec } = require("child_process");
+
+var colors = require("colors/safe");
+
+//
+// Setting these properties customizes the prompt.
+//
+
+anybar("exclamation");
+
+prompt.message = colors.rainbow("");
+prompt.delimiter = colors.green(" (0-9) ");
+
+prompt.start();
+
+let loop = function() {
+    exec("afplay /System/Library/Sounds/Glass.aiff")
+    exec("osascript -e 'display notification \"POSTURE!\"'")
+    anybar("question");
+    let timer = new Timer([{ interval: 1000, stpwatch: false }]);
+    prompt.get(
+        {
+            properties: {
+                howfar: {
+                    description: colors.green("Rate your posture (1-9), 0 to quit: ")
+                }
+            }
+        },
+        (err, res) => {
+            if (res.howfar === "0") {
+                return process.kill(process.pid);
+            }
+            timer.start(random.int(60000, Number(res.howfar) * 1000));
+            // run for x seconds
+        }
+    );
+
+    timer.on("tick", ms => {
+        anybar("red");
+    });
+
+    timer.on("done", () => {
+        console.log("done!");
+        console.log("\007");
+        timer.stop();
+        loop();
+    });
+
+    timer.on("statusChanged", status => console.log("status:", status));
+};
+
+loop();
+
+// run for x seconds
+// timer.stop()
+// timer.pause()
+// timer.resume()
+//events -> .on/done/statusChanged('tick'/'done'/'statusChanged', (ms/status) => {})
+//properties timer.time, timer.duration, timer.status
