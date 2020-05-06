24daacca698b0cda85d66aa4a22c160a2fc2f30c
suraj <surajsharma@pm.me>
2020-05-06 08:22:42 +0530

    added on, timeout handlers for notifier

--- index.js
+++ index.js
@@ -83,6 +83,14 @@ let loop = function () {
         wait: true,
     });
 
+    notifier.on("click", function (notifierObject, options, event) {
+        // Triggers if `wait: true` and user clicks notification
+    });
+
+    notifier.on("timeout", function (notifierObject, options) {
+        // Triggers if `wait: true` and notification closes
+    });
+
     anybar("question");
 
     let timer = new Timer([{ interval: 1000, stopwatch: false }]);
